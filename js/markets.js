ea587a1161ad294f26c27b6980a6bf956da526ed
gtamoro <gtamoro@gmail.com>
2016-10-16 00:01:18 +1100

    More styling changes

--- js/markets.js
+++ js/markets.js
@@ -7,6 +7,8 @@ function start() {
 function setupCalendar() {
   $('#calendar').fullCalendar( {
     googleCalendarApiKey: 'AIzaSyBTj_T3c8bnFq_awcTJ362dHCApossSNJQ',
+    eventBackgroundColor: '#478CCC', eventBorderColor: '#478CCC',
+    eventTextColor: 'white',
     events: {
       googleCalendarId: 'kt78h7amqeqbhssu8roljo4udo@group.calendar.google.com'
     }
