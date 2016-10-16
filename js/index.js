1468579eb3cb4dfce81848ef4684cef02b5ab06b
gtamoro <gtamoro@gmail.com>
2016-10-16 23:29:41 +1100

    Added hamburger and 650px breakpoint

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ function start() {
   $(document).scroll(toggleNavOpacity);
   $(window).resize(adjustSquarePanel);
   $(".newsletter-form").submit(signUp);
+  $("#menuButton").click(displayMenu);
 }
 
 // make the panels square
@@ -13,17 +14,22 @@ function adjustSquarePanel() {
   $('.panel').height($('.panel').width());
 }
 
-// make the navigation opaque when scrolling down, and solid when on top of screen.
+// make the navBar opaque when scrolling down, and solid when on top of screen.
 function toggleNavOpacity() {
   if($(window).scrollTop() === 0) {
-    $("#navigation").toggleClass("notTop",false);
-    $("#navigation").toggleClass("top", true);
+    $("#navBar").toggleClass("notTop",false);
+    $("#navBar").toggleClass("top", true);
   } else {
-    $("#navigation").toggleClass("top",false);
-    $("#navigation").toggleClass("notTop",true);
+    $("#navBar").toggleClass("top",false);
+    $("#navBar").toggleClass("notTop",true);
   }
 }
 
 function signUp(event) {
   event.preventDefault();
+}
+
+function displayMenu(event) {
+  event.preventDefault();
+  $("#menuList").slideToggle();
 }
